a2a4f8ad7194db4d366cf16d91f1a814203d7a63
Andrey Kovalchuk <aka.xzib1t@gmail.com>
2025-01-20 10:45:00 +0300

    +added tests of Agent

---
+++ library/awplflib/agent/tests.h
@@ -0,0 +1,178 @@
+#pragma once
+
+#include <numeric>
+#include <memory>
+
+#include "utils/object_pool.h"
+#include "utils/matrix.h"
+#include "agent/agent.h"
+
+#include "LFAgent.h"
+#include "LFScanners.h"
+
+namespace agent {
+
+	static size_t GetRand(size_t low_dist, size_t high_dist) {
+		static bool init_srand = false;
+		if (!init_srand) {
+			std::srand((unsigned int)std::time(nullptr));
+			init_srand = true;
+		}
+		return low_dist + std::rand() % (high_dist + 1 - low_dist);
+	}
+
+	class TRandomSupervisor : public ILFSupervisor
+	{
+	public:
+		TRandomSupervisor() {}
+		virtual TDetections Detect(std::shared_ptr<TLFImage> img) override {
+			
+			auto width = img->GetImage()->sSizeX;
+			auto height = img->GetImage()->sSizeY;
+
+			TDetections detections;
+			for (size_t i = 0; i < 10; i++) {
+				awpRect rect;
+				rect.left = GetRand(0, width - 5);
+				rect.right = GetRand(rect.left, width);
+				rect.top = GetRand(0, height - 5);
+				rect.bottom = GetRand(rect.top, height);
+				detections.push_back(rect);
+			}
+
+			return detections;
+		}
+
+	};
+
+
+	class TRandomDetector : public IDetector
+	{
+	public:
+		TRandomDetector() = default;
+
+		bool Initialize() {
+
+			scanner_ = std::make_unique<TLFScanner>();
+
+			size_t cascade_count = GetRand(3, 10);  // rand ot 3 do 10
+			size_t count = 0;
+			for (auto i = 0; i < cascade_count; i++) {
+				size_t feature_count = GetRand(10, 100); // rand ot 10 do 100
+				feature_count_list_.push_back(feature_count + count);
+				count = feature_count_list_.back();
+			}
+			tmap_ptr_ = std::make_shared<TFeatures::TMap>(feature_count_list_);
+			return (bool)tmap_ptr_;
+		}
+
+		void Detect(std::shared_ptr<TLFImage> img, TFeaturesBuilder& builder) {
+			float* data = builder.GetMutableData().GetRow(0);
+			size_t stride = builder.feats_count();
+			auto triggered = builder.GetTriggered();
+			auto map = builder.map();
+
+			size_t detected = 0;
+
+			for (auto i = 0; i < triggered.size(); i++) {
+				// With probability 0.99 it is negative result
+				if (GetRand(0, 100) > 1) {
+					size_t cascade_number = GetRand(0, map->size() - 1);
+					triggered[i] = cascade_number;
+					size_t count = 0;
+					for (auto j = 0; j < map->size(); j++) {
+						if (j > cascade_number) break;
+						size_t cur_cascade_lenght = map.get()->at(j);
+						SetRandData(data + (i * stride) + count, cur_cascade_lenght);
+						count += cur_cascade_lenght;
+					}
+				}
+				else { // fill all cascade
+					SetRandData(data + (i * stride), stride);
+					detected++;
+				}
+			}
+
+			std::thread::id this_id = std::this_thread::get_id();
+
+			std::stringstream ss;
+
+			ss << "Detection thread " << this_id
+				<< " frags_begin=" << builder.frags_begin()
+				<< " frags_end=" << builder.frags_end()
+				<< " detected=" << detected << std::endl;
+
+			(std::cout << ss.str()).flush();
+
+		}
+
+		virtual ILFScanner* GetScanner()  override {
+			return scanner_.get();
+		}
+
+		virtual std::unique_ptr<IWorker> CreateWorker() override;
+
+		virtual bool LoadXML(TiXmlElement* parent)  override {
+			return false;
+		}
+		virtual TiXmlElement* SaveXML()  override {
+			return nullptr;
+		}
+
+		virtual TFeatures::TMapPtr GetMap() override { return tmap_ptr_; }
+
+
+
+	private:
+		static void SetRandData(float* data, size_t size) {
+			for (size_t i = 0; i < size; i++)
+				data[i] = float(GetRand(1, 100));
+		}
+
+	private:
+		std::function<void(std::shared_ptr<TFeatures>)> callback_;
+		std::vector<size_t> feature_count_list_;
+		TFeatures::TMapPtr tmap_ptr_;
+
+		std::unique_ptr<ILFScanner> scanner_;
+
+	};
+
+
+	class TRandomWorker : public IWorker {
+	public:
+		TRandomWorker(TRandomDetector* detector) : detector_(detector) {}
+
+		virtual void Detect(std::shared_ptr<TLFImage> img, TFeaturesBuilder& builder) override {
+			assert(detector_);
+			detector_->Detect(img, builder);
+		}
+
+	private:
+		TRandomDetector* detector_ = nullptr;
+	};
+
+	std::unique_ptr<IWorker> TRandomDetector::CreateWorker() {
+		return std::make_unique<TRandomWorker>(this);
+	}
+
+
+	class TRandomAgent : public TLFAgent
+	{
+	public:
+		TRandomAgent() : TLFAgent() {
+		}
+
+		bool Initialize() {
+			auto detector = std::make_unique<TRandomDetector>();
+			if (detector->Initialize())
+				detector_ = std::move(detector);
+
+			return bool(detector_);
+
+		}
+
+	};
+
+}
+
--- tools/supervisor_correctors/supervisor_correctors.cpp
+++ tools/supervisor_correctors/supervisor_correctors.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "LFAgent.h"
+#include "agent/tests.h"
 
 // supervisor_correctors.exe path_to_xml path_to_jpeg
 int main(int argc, char* argv[]) {
@@ -11,13 +12,20 @@ int main(int argc, char* argv[]) {
     //std::string path_to_xml = argv[1];
     //std::string path_to_image = argv[2];
 
-    auto agent = CreateAgent();
+    agent::TRandomAgent agent;
+    if (!agent.Initialize())
+        return -1;
+        
+
+    auto sv = std::make_shared<agent::TRandomSupervisor>();
+    
     //if (!agent->LoadXML(path_to_xml.c_str())) {
     //if (!agent->LoadXML(nullptr)) {
     //    std::cout << "LoadXML return false for " << path_to_xml << std::endl;
     //    return -2;
     //}
 
+    // Setup random image
     awpImage* image = nullptr;
     awpCreateImage(&image, 1280, 720, 3, AWP_BYTE);
 
@@ -29,8 +37,8 @@ int main(int argc, char* argv[]) {
         std::cout << "LoadFromFile return false for " << path_to_image << std::endl;
         return -3;
     }*/
-    agent->SetSupervisor(CreateSupervisor());
-    agent->Detect(img);
+    agent.SetSupervisor(sv);
+    agent.Detect(img);
     return 0;
 }
 
