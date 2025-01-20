e0bcb72407031ad14fb99c38a0f98d5d86f08fea
swan911 <swan911@yandex.ru>
2025-01-20 12:03:47 +0300

    few image detect added

--- tools/supervisor_correctors/supervisor_correctors.cpp
+++ tools/supervisor_correctors/supervisor_correctors.cpp
@@ -4,41 +4,42 @@
 
 // supervisor_correctors.exe path_to_xml path_to_jpeg
 int main(int argc, char* argv[]) {
-    /*if (argc != 3) {
-        std::cout << "Usage: supervisor_correctors.exe path_to_xml path_to_jpeg" << std::endl;
-        return -1;
-    }*/
-
-    //std::string path_to_xml = argv[1];
-    //std::string path_to_image = argv[2];
 
     agent::TRandomAgent agent;
     if (!agent.Initialize())
         return -1;
-        
 
     auto sv = std::make_shared<agent::TRandomSupervisor>();
+    agent.SetSupervisor(sv);
+
+    if (argc != 3) {
+        std::cout << "Usage: supervisor_correctors.exe path_to_xml path_to_jpeg" << std::endl;
+        // Setup random image
+        awpImage* image = nullptr;
+        awpCreateImage(&image, 1280, 720, 3, AWP_BYTE);
+        std::shared_ptr<TLFImage> img = std::make_shared<TLFImage>();
+        img->SetImage(image);       
+        agent.Detect(img);
+        return 0;
+    }
+
+    std::string path_to_xml = argv[1];
+    std::string path_to_image = argv[2];
     
     //if (!agent->LoadXML(path_to_xml.c_str())) {
     //if (!agent->LoadXML(nullptr)) {
     //    std::cout << "LoadXML return false for " << path_to_xml << std::endl;
     //    return -2;
     //}
-
-    // Setup random image
-    awpImage* image = nullptr;
-    awpCreateImage(&image, 1280, 720, 3, AWP_BYTE);
-
     std::shared_ptr<TLFImage> img = std::make_shared<TLFImage>();
-
-    img->SetImage(image);
-
-    /*if (!img->LoadFromFile(path_to_image.c_str())) {
+    if (!img->LoadFromFile(path_to_image.c_str())) {
         std::cout << "LoadFromFile return false for " << path_to_image << std::endl;
         return -3;
-    }*/
-    agent.SetSupervisor(sv);
-    agent.Detect(img);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        agent.Detect(img);
+    }
     return 0;
 }
 
