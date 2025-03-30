987f400326defcf7dcc9af9ce722641b3613c5f9
olgashemagina <olgashemagina@gmail.com>
2025-03-30 14:34:19 +0400

    Config and example for making baseline detector added

---
+++ examples/build_face_ff0p1.bat
@@ -0,0 +1,4 @@
+echo off
+rem "Change Path to your Python installation folder."
+set PATH=../3rdparty/boost/lib;%LOCALAPPDATA%\Programs\Python\Python311;%PATH%
+..\build\bin\x64\Release\CSBuilder.exe -b "../models/face/csbuild_face_ms1000_ff0p3.xml"
